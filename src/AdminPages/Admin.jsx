5c2684e4e65238d9f79565f828720ca086ba47c4
vaibhav_rathore <vaibahvrathorema@gmail.com>
2024-03-29 09:05:16 +0530

    resolve error

--- src/AdminPages/Admin.jsx
+++ src/AdminPages/Admin.jsx
@@ -1,166 +1,62 @@
-// import React from "react"
-// import { Route, Routes } from "react-router"
-// import Allproduct from "../AdminComponents/product/Allproduct"
-// import AdminProductDetaills from "../AdminComponents/product/adminProductDetaills"
-// import UpdateProduct from "../AdminComponents/product/adminUpdateProduct"
-// import Adminsidebar from "../AdminComponents/slidebar/adminsidebar"
-// import AdminaddProduct from "../AdminComponents/product/adminaddProduct"
-// import Getallcatogory from "../AdminComponents/catogory/getallcatogory"
-// import Addcatogory from "../AdminComponents/catogory/addcatogory"
-// import Editcategories from "../AdminComponents/catogory/editcategories"
-// import Allorder from "../AdminComponents/Order/allorder"
-// import Orderdetails from "../AdminComponents/Order/orderdetails"
-// import Admindasboard from "../AdminComponents/dasboard/admindasboard"
-// import Allproduct from "../AdminComponents/product/Allproduct";
-// import AdminProductDetaills from "../AdminComponents/product/adminProductDetaills";
-// import UpdateProduct from "../AdminComponents/product/adminUpdateProduct";
-// import Adminsidebar from "../AdminComponents/slidebar/adminsidebar";
-// import AdminaddProduct from "../AdminComponents/product/adminaddProduct";
-// import Getallcatogory from "../AdminComponents/catogory/getallcatogory";
-// import Addcatogory from "../AdminComponents/catogory/addcatogory";
-// import Editcategories from "../AdminComponents/catogory/editcategories";
-// import Allorder from "../AdminComponents/Order/allorder";
-// import Orderdetails from "../AdminComponents/Order/orderdetails";
-// import Admindasboard from "../AdminComponents/dasboard/admindasboard";
-// import OfferPage from "../AdminComponents/email/OfferTemplate";
-// import GetallCoupan from "../AdminComponents/Coupan/coupanallcatogory";
-// import AddCoupan from "../AdminComponents/Coupan/coupanCreate";
-// import EditCoupan from "../AdminComponents/Coupan/coupanUpdate";
-
-// function Admin() {
-// 	return (
-// 		<div className="main_admin_pages">
-// 			<div className="admin_page_sidebar_div">
-// 				<Adminsidebar />
-// 			</div>
-// 			<div className="admin_page_main_div">
-// 				<Routes>
-// 					<Route
-// 						path="/sidebar"
-// 						element={<Adminsidebar />}
-// 					/>
-// 					{/* products */}
-// 					<Route
-// 						path="/allproducts"
-// 						element={<Allproduct />}
-// 					/>
-// 					<Route
-// 						path="/add-product"
-// 						element={<AdminaddProduct />}
-// 					/>
-// 					<Route
-// 						path="/product-details/:productId"
-// 						element={<AdminProductDetaills />}
-// 					/>
-// 					<Route
-// 						path="/product-update/:productId"
-// 						element={<UpdateProduct />}
-// 					/>
-
-// 					{/* category */}
-// 					<Route
-// 						path="/all-categories"
-// 						element={<Getallcatogory />}
-// 					/>
-// 					<Route
-// 						path="/add-category"
-// 						element={<Addcatogory />}
-// 					/>
-// 					<Route
-// 						path="/category-update/:Id"
-// 						element={<Editcategories />}
-// 					/>
-
-//           {/* orders */}
-//           <Route path="/all-orders" element={<Allorder />} />
-//           <Route path="/order/:id" element={<Orderdetails/>} />
-
-//           {/* coupan */}
-//           <Route path="/All-coupan" element={<GetallCoupan />} />
-//           <Route path="/add-coupan" element={<AddCoupan />} />
-//           <Route path="/update-coupan" element={<EditCoupan />} />
-
-// 					{/* admin */}
-// 					<Route
-// 						path="/admin-dashboard"
-// 						element={<Admindasboard />}
-// 					/>
-// 			</div>
-//           {/* admin */}
-//           <Route path="/admin-dashboard" element={<Admindasboard />} />
-
-
-//           {/* email send */}
-//           <Route path="/send-email" element={<OfferPage />} />
-//         </Routes>
-// 		</div>
-//   );
-// }
-
-// export default Admin
-
-
-
-
-
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router";
+
 import Allproduct from "../AdminComponents/product/Allproduct";
-import AdminProductDetails from "../AdminComponents/product/adminProductDetails";
+import AdminProductDetaills from "../AdminComponents/product/adminProductDetaills";
 import UpdateProduct from "../AdminComponents/product/adminUpdateProduct";
 import Adminsidebar from "../AdminComponents/slidebar/adminsidebar";
 import AdminaddProduct from "../AdminComponents/product/adminaddProduct";
-import GetallCategory from "../AdminComponents/catogory/getallcatogory";
-import AddCategory from "../AdminComponents/catogory/addCategory";
-import EditCategories from "../AdminComponents/catogory/editCategories";
+import Getallcatogory from "../AdminComponents/catogory/getallcatogory";
+import Addcatogory from "../AdminComponents/catogory/addcatogory";
+import Editcategories from "../AdminComponents/catogory/editcategories";
 import Allorder from "../AdminComponents/Order/allorder";
 import Orderdetails from "../AdminComponents/Order/orderdetails";
-import Admindashboard from "../AdminComponents/dashboard/admindashboard";
+import Admindasboard from "../AdminComponents/dasboard/admindasboard";
 import OfferPage from "../AdminComponents/email/OfferTemplate";
-import GetallCoupon from "../AdminComponents/Coupon/couponallcategory";
-import AddCoupon from "../AdminComponents/Coupon/couponCreate";
-import EditCoupon from "../AdminComponents/Coupon/couponUpdate";
+import GetallCoupan from "../AdminComponents/Coupan/coupanallcatogory";
+import AddCoupan from "../AdminComponents/Coupan/coupanCreate";
+import EditCoupan from "../AdminComponents/Coupan/coupanUpdate";
 
 function Admin() {
   return (
-    <div className="main_admin_pages">
-      <div className="admin_page_sidebar_div">
+    <div className="main_admin_pages" >
+      <div className="admin_page_sidebar_div" >
         <Adminsidebar />
       </div>
-      <div className="admin_page_main_div">
+      <div className="admin_page_main_div" >
         <Routes>
           <Route path="/sidebar" element={<Adminsidebar />} />
-
-          {/* Products */}
+          {/* products */}
           <Route path="/allproducts" element={<Allproduct />} />
           <Route path="/add-product" element={<AdminaddProduct />} />
           <Route
             path="/product-details/:productId"
-            element={<AdminProductDetails />}
+            element={<AdminProductDetaills />}
           />
           <Route
             path="/product-update/:productId"
             element={<UpdateProduct />}
           />
 
-          {/* Categories */}
-          <Route path="/all-categories" element={<GetallCategory />} />
-          <Route path="/add-category" element={<AddCategory />} />
-          <Route path="/category-update/:Id" element={<EditCategories />} />
+          {/* category */}
+          <Route path="/all-categories" element={<Getallcatogory />} />
+          <Route path="/add-category" element={<Addcatogory />} />
+          <Route path="/category-update/:Id" element={<Editcategories />} />
 
-          {/* Orders */}
+          {/* orders */}
           <Route path="/all-orders" element={<Allorder />} />
-          <Route path="/order/:id" element={<Orderdetails />} />
+          <Route path="/order/:id" element={<Orderdetails/>} />
 
-          {/* Coupons */}
-          <Route path="/All-coupon" element={<GetallCoupon />} />
-          <Route path="/add-coupon" element={<AddCoupon />} />
-          <Route path="/update-coupon" element={<EditCoupon />} />
+          {/* coupan */}
+          <Route path="/All-coupan" element={<GetallCoupan />} />
+          <Route path="/add-coupan" element={<AddCoupan />} />
+          <Route path="/update-coupan" element={<EditCoupan />} />
 
-          {/* Admin Dashboard */}
-          <Route path="/admin-dashboard" element={<Admindashboard />} />
+          {/* admin */}
+          <Route path="/admin-dashboard" element={<Admindasboard />} />
 
-          {/* Email Send */}
+
+          {/* email send */}
           <Route path="/send-email" element={<OfferPage />} />
         </Routes>
       </div>
