7c89451a411cae31ff4053f1768e9efb2daed87e
Kiran Katti <139916130+kirankattii@users.noreply.github.com>
2024-04-02 18:45:23 +0530

    done

--- src/components/productDropdown/ProductDropdown.jsx
+++ src/components/productDropdown/ProductDropdown.jsx
@@ -1,39 +1,20 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { Link } from "react-router-dom"
 
 const ProductDropdown = () => {
-	const [categories, setCategories] = useState([])
-	useEffect(() => {
-		async function fetchCategories() {
-			try {
-				const response = await makeApi("/api/get-all-categories", "GET")
-				if (response.status === 200) {
-					setCategories(response.data.categories)
-				}
-			} catch (error) {
-				console.log("Error fetching categories:", error)
-			} finally {
-			}
-		}
-		fetchCategories()
-	}, [])
-
 	return (
 		<div className="product-dropdown">
 			{" "}
 			<ul className="drop-down-menu">
-				{categories.map((items, i) => (
-					<li className="dropdown-li">
-						<Link to="/products">{items.name}</Link>
-					</li>
-				))}
-
-				{/* <li className="dropdown-li">
+				<li className="dropdown-li">
+					<Link to="/products">Gift Hamper</Link>
+				</li>
+				<li className="dropdown-li">
 					<Link to="/products/savory">Savory</Link>
 				</li>
 				<li className="dropdown-li">
 					<Link to="/products/biscuits">Biscuits</Link>
-				</li> */}
+				</li>
 			</ul>
 		</div>
 	)
