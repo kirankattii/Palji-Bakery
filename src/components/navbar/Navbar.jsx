fc3a6eb4ab0583de0eca8757e58b69b7eb821284
vaibhav_rathore <vaibahvrathorema@gmail.com>
2024-04-06 12:53:45 +0530

    update my addres and navbar login button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -82,7 +82,6 @@ const Navbar = () => {
 	}
 	// fetchData()
 	// }, [])
-	console.log(products)
 
 	const handleChange = (value) => {
 		setInput(value), fetchData(value)
@@ -103,7 +102,7 @@ const Navbar = () => {
 	return showNavbar ? (
 		<div className="navbar">
 			<div className="left-navbar">
-				{isloggedIn ? (
+				{isloggedIn === true ? (
 					<div>
 						<img
 							onClick={() => setOpenProfile((prev) => !prev)}
